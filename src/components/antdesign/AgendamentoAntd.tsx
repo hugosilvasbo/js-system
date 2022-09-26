368de727a3fe89e33324222422bd0cbd8fe77951
Hugo Souza <hugo_sbo04@hotmail.com>
2022-09-26 01:03:30 -0300

    Trabalhando com o componente de agendamento

--- src/components/antdesign/AgendamentoAntd.tsx
+++ src/components/antdesign/AgendamentoAntd.tsx
@@ -10,26 +10,28 @@ import moment, { Moment } from 'moment';
 import React from 'react';
 import jconst from '../../assets/jsConstantes.json';
 
-const _groupBy = 'DDMMYYYY'
+const keyFormat = 'DDMMYYYY'
 
 /**
- * Obtém os agendamentos do mês agrupados por data.
+ * Obtém os agendamentos do mês agrupados por data (DDMMYYYY).
  * @function
  * @param {Moment} value - período a ser obtido.
  */
 async function getScheduling(value: Moment) {
   let clone = value.clone()
-  let start = clone.startOf('month').format('YYYY-MM-DD')
-  let end = clone.endOf('month').format('YYYY-MM-DD')
+
+  let first_day_of_month = clone.startOf('month').format('YYYY-MM-DD')
+  let last_day_of_month = clone.endOf('month').format('YYYY-MM-DD')
 
   let res = await axios.get(`${jconst.url_api_barber}schedule`, {
     params: {
-      startdate: start,
-      enddate: end
+      startdate: first_day_of_month,
+      enddate: last_day_of_month
     }
   })
 
-  return _.groupBy(res.data, (r: any) => moment(r.date).format(_groupBy));
+  let _groupByDay = _.groupBy(res.data, (r: any) => moment(r.date).format(keyFormat))
+  return _groupByDay;
 }
 
 class Agendamento extends React.Component<{}, any> {
@@ -37,46 +39,34 @@ class Agendamento extends React.Component<{}, any> {
     schedule: {}
   }
 
+  style = { fontSize: '12px', marginBottom: '12px', listStyleType: 'none' }
+
   async componentDidMount(): Promise<void> {
     let data = await getScheduling(moment('2022-09-01'))
     this.setState({ schedule: data })
   }
 
+  /**
+  * Renderiza as células do calendário.
+  * Aqui, é feito um filtro pela key (usando o pick), exemplo: 20220910 -> Traz apenas os registros do dia/mês/ano
+  * Com isto, é percorrido os elementos e mapeando o mesmo.
+  * @function
+  * @param {Moment} value - período a ser obtido.
+  */
   dateCellRender = (value: Moment) => {
-    let key = value.format(_groupBy)
-    //console.log(value.date())
-
-    // exemplo: key = 21092022
-    // no objeto agrupado por data, fazemos o filtro pela key
-    // trazendo apenas os resultados do dia
-    let pick = _.pick(this.state.schedule, key)
-    console.log(pick)
-
-    // percorrer as keys
-    // e para cada uma, se igual a key let
-    // percorrer, guardar num obj e setar
+    let key = value.format(keyFormat)
+    let scheduleInTheMonth = _.pick(this.state.schedule, key)
 
     return (
-      <ul>
-        {
-        }
-      </ul>
-
-      /*_.map(this.state.schedule, (s: ISchedule) => {
-      let _date = moment(s.date).format('DD/MM/YYYY')
-    let _value = value.format('DD/MM/YYYY')
-
-    return <>
-      {
-        _date === _value &&
-        <li style={{ fontSize: '12px', marginBottom: '9px' }}>
-          {s.date + ': ' + s.person.name}
-        </li>
-      }
-    </>
-      })
-    }*/
-
+      _.map(scheduleInTheMonth, schedule =>
+        <ul key={schedule} onClick={() => console.log({ exemploClique: schedule })}>
+          {_.map(schedule, (item: any) => {
+            return <li style={this.style}>
+              {`${item.date} - ${item.person?.name}`}
+            </li>
+          })}
+        </ul>
+      )
     );
   }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 
 const Home = () => {
     return (
-        <>
-            
-        </>
+        <ul>
+            <li>
+                O que fazer no agendamento:
+                Corrigir questão dos dias que está + 1.
+                Formatar o horário + Nome da pessoa apenas.
+            </li>
+        </ul>
     )
 }
 
