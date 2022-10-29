c475f53e49aea62e1212625f754ce9fc1fb6d2b5
Hugo S <hugo_sbo04@hotmail.com>
2022-10-28 23:02:53 -0300

    Alteraçao do agendamento api

--- src/components/antdesign/AgendamentoAntd.tsx
+++ src/components/antdesign/AgendamentoAntd.tsx
@@ -8,8 +8,9 @@ import Card from 'antd/lib/card/Card';
 import axios from 'axios';
 import _ from 'lodash';
 import moment, { Moment } from 'moment';
-import React from 'react';
-import jconst from '../../assets/jsConstantes.json'
+import React, { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import jconst from '../../assets/jsConstantes.json';
 
 async function buscarNaAPIOsAgendamentosDoMes(value: Moment) {
   let clone = value.clone()
@@ -30,24 +31,24 @@ async function buscarNaAPIOsAgendamentosDoMes(value: Moment) {
   return agrupadoPorDia;
 }
 
-class Agendamento extends React.Component<{}, any> {
-  state = {
-    dados: {},
-    agendamentosNoDia: {} as any,
-    agendamentoSelecionado: {} as any,
-    modal: {
-      openModal: false
+const Agendamento = () => {
+  const [dados, setDados] = useState({} as any);
+  const [agendamentosNoDia, setAgendamentosNoDia] = useState({} as any);
+  const [agendamentoSelecionado, setAgendamentoSelecionado] = useState({} as any);
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let dados = await buscarNaAPIOsAgendamentosDoMes(moment(new Date()))
+      setDados(dados)
     }
-  }
 
-  async componentDidMount(): Promise<void> {
-    let data = await buscarNaAPIOsAgendamentosDoMes(moment(new Date()));
-    this.setState({ dados: data });
-  }
+    fetchData().catch(console.error)
+  }, [])
 
-  dateCellRender = (value: Moment) => {
+  const dateCellRender = (value: Moment) => {
     let data = value.format('DDMMYYYY')
-    let agendamentos = _.pick(this.state.dados, data)
+    let agendamentos = _.pick(dados, data)
 
     const FragListaHTML = (props: any) => {
       const css = { fontSize: '12px', marginBottom: '12px', listStyleType: 'none' }
@@ -62,76 +63,80 @@ class Agendamento extends React.Component<{}, any> {
 
     return (
       _.map(agendamentos, (agendamento: any) =>
-        <ul key={agendamento} onClick={() => this.setState({ agendamentosNoDia: agendamento })}  >
-          {_.map(agendamento, (a: any) =>
-            <FragListaHTML
-              _id={a._id}
-              person={a.person.name}
-              date={a.date}
-            />
-          )}
+        <ul key={agendamento} onClick={() => setAgendamentosNoDia(agendamento)}  >
+          {
+            _.map(agendamento, (a: any) =>
+              <FragListaHTML _id={a._id} person={a.person.name} date={a.date} />
+            )
+          }
         </ul>
       )
     );
   }
 
-  onPanelChange = async (value: Moment) => {
-    let res = await buscarNaAPIOsAgendamentosDoMes(value);
-    this.setState({ dados: res })
-  }
-
-  FragAgendamentosNoDia = () => {
-    const stateSelecionado = this.state.agendamentoSelecionado;
-    const stateModal = this.state.modal;
-    const stateAgendadoNoDia = this.state.agendamentosNoDia;
-
+  const FragAgendamentosNoDia = () => {
     const [form_digitacao] = Form.useForm();
-    form_digitacao.setFieldsValue(stateSelecionado);
+    form_digitacao.setFieldsValue(agendamentoSelecionado);
 
     const onSubmitForm = async () => {
-      form_digitacao.validateFields()
-        .then(async (value: any) => {
-          let res = await axios.post(`${jconst.url_api_barber}schedule`, value)
-            .then((value: any) => {
-              console.log({ resultadoAPI: value })
+      await form_digitacao.validateFields()
+        .then(async (res) => {
+          let _url = `${jconst.url_api_barber}schedule/${res._id}`
+
+          console.log({ _url })
+          console.log({ res })
+
+          await axios.patch(_url, res)
+            .then((res: any) => {
+              toast.success(res.message)
             })
-            .catch((reason: any) => {
-              console.log({ falha: reason });
+            .catch((err: any) => {
+              toast.error("Falha na atualização...")
             })
-        });
+        })
+        .catch((err: any) => {
+          toast.error("Falha na validação do formulário")
+        })
     }
 
     return <>
       {
         <Space direction={'vertical'} size={'middle'} style={{ display: 'flex' }} >
           {
-            _.map(stateAgendadoNoDia, (agendamento: any) => {
+            _.map(agendamentosNoDia, (agendamento: any) => {
               return <>
                 <Card
                   title={agendamento.person.name}
                   size="small"
                   headStyle={{ background: '#f1f1f1' }}
-                  onClick={() => { this.setState({ modal: { openModal: true, }, agendamentoSelecionado: agendamento }) }}
-                >
+                  onClick={() => {
+                    setOpenModal(true)
+                    setAgendamentoSelecionado(agendamento)
+                  }}>
                   <p>{agendamento.date}</p>
                   <p>{agendamento.cellphone}</p>
                 </Card>
               </>
             })
           }
+
           <Modal
             title={"Manutenção"}
             centered
-            open={stateModal.openModal}
+            open={openModal}
             onOk={onSubmitForm}
-            onCancel={() => this.setState({ modal: { openModal: false } })}
+            onCancel={() => setOpenModal(false)}
             width={1000}
             okText={"Gravar"}
             cancelText={"Sair"}
+            forceRender
           >
             <Form form={form_digitacao} layout={"vertical"}>
               <Row>
                 <Col span={24} >
+                  <Form.Item label={'ID'} name={'_id'}>
+                    <Input disabled={true} />
+                  </Form.Item>
                   {/** exemplo de como mostrar conteudo de sub objetos */}
                   <Form.Item label={"Razão"} name={['person', 'name']}>
                     <Input disabled={true} />
@@ -149,31 +154,33 @@ class Agendamento extends React.Component<{}, any> {
                 </Form.Item>
               </Col>
             </Form>
+            <ToastContainer />
           </Modal>
         </Space>
       }
     </>
   }
 
-  render() {
-    return <>
-      <Row>
-        <Col span={18}>
-          <Calendar
-            locale={locale}
-            dateCellRender={this.dateCellRender}
-            onPanelChange={this.onPanelChange}
-          />
-        </Col>
-        <Col span={6} style={{ padding: '1em' }}>
-          <Typography.Title style={{ width: '100%', textAlign: 'center', marginBottom: '1em' }} level={5} >
-            Agendamento(s) no dia
-          </Typography.Title>
-          <this.FragAgendamentosNoDia />
-        </Col>
-      </Row>
-    </>
-  }
+  return <>
+    <Row>
+      <Col span={18}>
+        <Calendar
+          locale={locale}
+          dateCellRender={dateCellRender}
+          onPanelChange={async (value) => {
+            let res = await buscarNaAPIOsAgendamentosDoMes(value)
+            setDados(res)
+          }}
+        />
+      </Col>
+      <Col span={6} style={{ padding: '1em' }}>
+        <Typography.Title style={{ width: '100%', textAlign: 'center', marginBottom: '1em' }} level={5} >
+          Agendamento(s) no dia
+        </Typography.Title>
+        <FragAgendamentosNoDia />
+      </Col>
+    </Row>
+  </>
 };
 
 export default Agendamento;
