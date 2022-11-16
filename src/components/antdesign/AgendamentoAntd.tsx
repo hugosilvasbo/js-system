b3851e331c1c97360d8fe3341d552a513b6a2d1b
Hugo S <hugo_sbo04@hotmail.com>
2022-11-16 00:36:26 -0300

    Ajustes

--- src/components/antdesign/AgendamentoAntd.tsx
+++ src/components/antdesign/AgendamentoAntd.tsx
@@ -1,5 +1,5 @@
 import { CloseOutlined } from '@ant-design/icons';
-import { Calendar, Col, Form, Input, Modal, Row, Spin, Table, Tooltip } from 'antd';
+import { Calendar, Col, Form, Input, Modal, Row, Table, Tooltip } from 'antd';
 import locale from 'antd/es/date-picker/locale/pt_BR';
 import Card from 'antd/lib/card/Card';
 import { ColumnsType } from 'antd/lib/table';
@@ -31,6 +31,16 @@ interface IModal {
   agendamentoID?: any
 }
 
+interface TipoDado {
+  key: string;
+  schedule_time: string,
+  client: string,
+  situation: {
+    description: string,
+    color: string
+  }
+}
+
 async function buscarNaAPIOsAgendamentosDoMes(value: Moment) {
 
   const _getFiltro = (_primeiro: boolean) => {
@@ -54,7 +64,6 @@ const Agendamento = () => {
   const [dados, setDados] = useState({} as any);
   const [showModal, setShowModal] = useState(undefined as unknown as IModal)
   const [agendamentosDoDia, setAgendamentosDoDia] = useState({} as any)
-  const [loading, setLoading] = useState({ descritivo: "", visivel: false })
   const [layoutPadrao, setLayoutPadrao] = useState(true);
   const [hideSidebar, setHideSidebar] = useState(true);
 
@@ -68,12 +77,12 @@ const Agendamento = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      const _idtoast = toast.loading("Buscando agendamentos...");
       try {
-        setLoading({ descritivo: "Consultando agendamentos...", visivel: true })
         let dados = await buscarNaAPIOsAgendamentosDoMes(moment(new Date()))
         setDados(dados);
       } finally {
-        setLoading({ descritivo: "", visivel: false })
+        toast.dismiss(_idtoast);
       }
     }
 
@@ -122,15 +131,18 @@ const Agendamento = () => {
     const _onSubmitForm = async () => {
       await form_digitacao.validateFields()
         .then(async (values: any) => {
-          var _agendamento = new AgendamentoModal(values, values._id);
-
-          await _agendamento.send()
-            .then((res: any) => toast.success(res.data.message))
-            .catch((e: any) => toast.error("" + e))
-            .finally(() => {
-              setLoading({ descritivo: "", visivel: false })
-              setShowModal({ showModal: EnTipoModal.mIndefinido })
-            })
+          const _agendamento = new AgendamentoModal(values, values._id);
+          const _idtoast = toast.loading("Processando...");
+          try {
+            await _agendamento.send()
+              .then((res: any) => toast.success(res.data.message))
+              .catch((e: any) => toast.error("" + e))
+              .finally(() => {
+                setShowModal({ showModal: EnTipoModal.mIndefinido })
+              })
+          } finally {
+            toast.dismiss(_idtoast);
+          }
         }).catch((e) => toast.error(e))
     }
 
@@ -186,97 +198,21 @@ const Agendamento = () => {
     </>
   }
 
-  const SidebarSelecao = () => {
-    /*const Manutencao = (props: any) => {
-
-
-      const _onClickDelete = () => {
-        setShowModal({ showModal: EnTipoModal.mAbrirModalConfirmExclusao, agendamentoID: props.agendamento._id })
-      }
-
-      
-      const _extra = <>
-        <Tooltip placement='left' title={'Excluir'}>
-          <DeleteOutlined style={{ color: 'red' }} onClick={_onClickDelete} />
-        </Tooltip>
-      </>
-
-      return <>
-
-      </>
-    }*/
-
-    const _extraCardSidebar = <>
-      <Tooltip placement='bottom' title={'Fechar o painel'}>
-        <CloseOutlined onClick={() => setHideSidebar(true)} />
-      </Tooltip>
-    </>
-
-    return <>
-      {
-        <Card
-          type='inner'
-          size='small'
-          title={`Detalhes (${agendamentosDoDia.length})`}
-          className="wrapper-sidebar-selection"
-          extra={_extraCardSidebar} >
-          {
-            _.map(agendamentosDoDia, (agendamento: any) => {
-              const _onClickItem = () => {
-                form_digitacao.setFieldsValue(agendamento);
-                setShowModal({ showModal: EnTipoModal.mManutencao })
-              }
-
-              return <>
-                <Tooltip title={`Clique para alterar - ${agendamento.situation?.description}`}>
-                  <Row onClick={_onClickItem} className="sidebar-selection-item" style={{ borderLeft: `4px solid ${agendamento.situation?.color}` }}>
-                    {DateUtils.dateFormatHHmm(agendamento.date) + ' ' + agendamento.person?.name}
-                  </Row>
-                </Tooltip>
-              </>
-            })
-          }
-        </Card>
-      }
-    </>
-  }
-
-  const FrameBotoesPrincipais = () => {
-    console.log("Frame cad buttons cai aqui")
-    const _callback = (_opcao: enBotoes) => {
-      switch (_opcao) {
-        case enBotoes.eNovo:
-          form_digitacao.resetFields();
-          setShowModal({ showModal: EnTipoModal.mInclusao })
-          break;
-        case enBotoes.eProcurar:
-          setLayoutPadrao(!layoutPadrao);
-          break;
-      }
-    }
-
-    return (
-      <FrameCadButtons
-        inEdition={false}
-        callbackClick={(e: enBotoes) => _callback(e)}
-        orientation={'horizontal'}
-        invisible={[enBotoes.eAlterar, enBotoes.eExcluir, enBotoes.eGravar, enBotoes.eCancelar]}
-        tooltipCaption={[{ button: enBotoes.eProcurar, caption: "Mudar modo de visualização" }]}
-      />
-    );
-  }
-
   const ModalDeConfirmacaoDeExclusao = () => {
     const _excluirAgendamento = async (_id: any) => {
-      setLoading({ descritivo: "Excluindo agendamento...", visivel: true })
-      var _agendamento = new AgendamentoModal({}, _id);
-      await _agendamento.delete()
-        .then((res: any) => toast.success(res.data.message))
-        .catch((e: any) => toast.error('Falha ao excluir...'))
-        .finally(() => {
-          setLoading({ descritivo: "", visivel: false })
-          setShowModal({ showModal: EnTipoModal.mIndefinido })
-        })
+      const _agendamento = new AgendamentoModal({}, _id);
+      const _idtoast = toast.loading("Excluindo agendamento...");
+      try {
+        await _agendamento.delete()
+          .then((res: any) => toast.success(res.data.message))
+          .catch((e: any) => toast.error('Falha ao excluir...'))
+          .finally(() => {
+            //setLoading({ descritivo: "", visivel: false })
+            setShowModal({ showModal: EnTipoModal.mIndefinido })
+          })
+      } finally {
+        toast.dismiss(_idtoast);
+      }
     }
 
     const _callback = (_opcaoSelecionada: EnRetorno) => {
@@ -296,160 +232,185 @@ const Agendamento = () => {
       callback={_callback} />
   }
 
-  const MontagemDoConteudo = () => {
-    console.log("Está redenrizando multiplas vezes...")
-    const panelChange = async (value: Moment) => {
-      let dados = await buscarNaAPIOsAgendamentosDoMes(value);
-      setDados(dados);
-    }
+  function dtSourceModeloTabela() {
+    let horarios: any = [];
 
-    const Detalhado = () => {
-      interface TipoDado {
-        key: string;
-        schedule_time: string,
-        client: string,
-        situation: {
-          description: string,
-          color: string
-        }
-      }
+    /***
+     * Passar aqui o dia atual clicado...
+     */
+    let horarioDeTrabalho = new Date('2022-11-17' + "T00:00:00");
+    horarioDeTrabalho.setHours(8, 0, 0, 0);
 
-      const dtSource = () => {
-        let horarios: any = [];
+    let horarioDoFinalDoExpediente = new Date('2022-11-17' + "T00:00:00");
+    horarioDoFinalDoExpediente.setHours(19, 30, 0, 0);
 
-        /***
-         * Passar aqui o dia atual clicado...
-         */
-        let horarioDeTrabalho = new Date('2022-11-17' + "T00:00:00");
-        horarioDeTrabalho.setHours(8, 0, 0, 0);
+    let _variacaoInicialMinutos = 30;
 
-        let horarioDoFinalDoExpediente = new Date('2022-11-17' + "T00:00:00");
-        horarioDoFinalDoExpediente.setHours(19, 30, 0, 0);
+    while (horarioDeTrabalho.getTime() < horarioDoFinalDoExpediente.getTime()) {
+      let _variacao = _variacaoInicialMinutos;
 
-        let _variacaoInicialMinutos = 30;
+      /**
+       * Para incluir o agendamento na lista, deve-se:
+       * - Conforme a montagem do horário do looping, analisar se há horários agendados no dia para setagem de valor. 
+       * - Portanto, desde que esse ID não esteja incluso no Json "horarios", por conta do looping que pode cair no mesmo horário,
+       *   e duplicar o registro (é errado).
+       * - Se tudo ocorrer bem, o procedimento de inclusão é feito normalmente.
+       */
 
-        while (horarioDeTrabalho.getTime() < horarioDoFinalDoExpediente.getTime()) {
-          let _variacao = _variacaoInicialMinutos;
+      let _filtrarHorarios = _.filter(agendamentosDoDia, (agendamento: any) => {
+        let _jaIncluso = _.filter(horarios, (horario: any) => horario.key === agendamento._id);
 
-          /**
-           * Para incluir o agendamento na lista, deve-se:
-           * - Conforme a montagem do horário do looping, analisar se há horários agendados no dia para setagem de valor. 
-           * - Portanto, desde que esse ID não esteja incluso no Json "horarios", por conta do looping que pode cair no mesmo horário,
-           *   e duplicar o registro (é errado).
-           * - Se tudo ocorrer bem, o procedimento de inclusão é feito normalmente.
-           */
+        return (new Date(agendamento.date).getHours() === horarioDeTrabalho.getHours()) && (_jaIncluso.length <= 0);
+      });
 
-          let _filtrarHorarios = _.filter(agendamentosDoDia, (agendamento: any) => {
-            let _jaIncluso = _.filter(horarios, (horario: any) => horario.key === agendamento._id);
+      if (_filtrarHorarios.length > 0) {
+        _.map(_filtrarHorarios, (value: any) => {
+          let _horarioInicial = new Date(value.date);
+          let _horarioFinal = new Date(value.date_end);
 
-            return (new Date(agendamento.date).getHours() === horarioDeTrabalho.getHours()) && (_jaIncluso.length <= 0);
+          _variacao = DateUtils.obterVariacaoMinutosEntreDatas(horarioDeTrabalho, _horarioFinal);
+
+          horarios.push({
+            schedule_time: DateUtils.dateFormatHHmm(_horarioInicial) + ' ' + DateUtils.dateFormatHHmm(_horarioFinal),
+            client: value.person.name,
+            situation: value.situation,
+            key: value._id,
           });
 
-          if (_filtrarHorarios.length > 0) {
-            _.map(_filtrarHorarios, (value: any) => {
-              let _horarioInicial = new Date(value.date);
-              let _horarioFinal = new Date(value.date_end);
-
-              _variacao = DateUtils.obterVariacaoMinutosEntreDatas(horarioDeTrabalho, _horarioFinal);
-
-              horarios.push({
-                schedule_time: DateUtils.dateFormatHHmm(_horarioInicial) + ' ' + DateUtils.dateFormatHHmm(_horarioFinal),
-                client: value.person.name,
-                situation: value.situation,
-                key: value._id,
-              });
-
-              horarioDeTrabalho.setTime(_horarioFinal.getTime())
-            });
-          }
-          else {
-            horarios.push({
-              schedule_time: DateUtils.dateFormatHHmm(horarioDeTrabalho),
-              client: "",
-              situation: { description: "Livre", color: "rgb(139 139 139)" },
-              key: new Date().getTime(),
-            });
-
-            horarioDeTrabalho.setMinutes(horarioDeTrabalho.getMinutes() + _variacao);
-          }
-        }
-
-        return horarios;
+          horarioDeTrabalho.setTime(_horarioFinal.getTime())
+        });
       }
+      else {
+        horarios.push({
+          schedule_time: DateUtils.dateFormatHHmm(horarioDeTrabalho),
+          client: "",
+          situation: { description: "Livre", color: "rgb(139 139 139)" },
+          key: new Date().getTime(),
+        });
 
-      const colunas: ColumnsType<TipoDado> = [
-        {
-          title: 'Horário',
-          dataIndex: 'schedule_time',
-          key: 'schedule_time',
-          width: "5%",
-          render(text, record) {
-            return <span style={{ color: record.situation?.color }}>{record.schedule_time}</span>;
-          }
-        },
-        {
-          title: 'Cliente',
-          dataIndex: 'client',
-          key: 'client',
-          width: "75%",
-          render(text, record) {
-            return <span style={{ color: record.situation?.color }}>{record.client}</span>;
-          }
-        },
-        {
-          title: 'Situação',
-          dataIndex: ['situation', 'description'],
-          key: 'situation',
-          width: "20%",
-          render(text, record) {
-            return <span style={{ color: record.situation?.color }}>{record.situation?.description}</span>;
-          }
-        }
-      ];
-
-      return <>
-        <Table
-          pagination={false}
-          columns={colunas}
-          style={{ padding: '10px' }}
-          dataSource={dtSource()}
-          onRow={(data: TipoDado) => {
-            return {
-              onClick: event => {
-                console.log({ ClickTableManutencao: data })
-                //form_digitacao.setFieldsValue(data);
-                //setShowModal({ showModal: EnTipoModal.mManutencao })
-              }
-            }
-          }}
-        />
-      </>
+        horarioDeTrabalho.setMinutes(horarioDeTrabalho.getMinutes() + _variacao);
+      }
     }
 
-    if (layoutPadrao) {
-      return <Calendar locale={locale} dateCellRender={dateCellRender} onPanelChange={panelChange} style={{ padding: '10px' }} />;
-    } else {
-      return <Detalhado />
+    return horarios;
+  }
+
+  async function panelChange(value: Moment) {
+    console.log("Calendar panel change")
+    let dados = await buscarNaAPIOsAgendamentosDoMes(value);
+    setDados(dados);
+  }
+
+  const _callbackBotoes = (_opcao: enBotoes) => {
+    switch (_opcao) {
+      case enBotoes.eNovo:
+        form_digitacao.resetFields();
+        setShowModal({ showModal: EnTipoModal.mInclusao })
+        break;
+      case enBotoes.eProcurar:
+        setLayoutPadrao(!layoutPadrao);
+        break;
     }
   }
 
+  const colunasModeloTabela: ColumnsType<TipoDado> = [
+    {
+      title: 'Horário',
+      dataIndex: 'schedule_time',
+      key: 'schedule_time',
+      width: "5%",
+      render(text, record) {
+        return <span style={{ color: record.situation?.color }}>{record.schedule_time}</span>;
+      }
+    },
+    {
+      title: 'Cliente',
+      dataIndex: 'client',
+      key: 'client',
+      width: "75%",
+      render(text, record) {
+        return <span style={{ color: record.situation?.color }}>{record.client}</span>;
+      }
+    },
+    {
+      title: 'Situação',
+      dataIndex: ['situation', 'description'],
+      key: 'situation',
+      width: "20%",
+      render(text, record) {
+        return <span style={{ color: record.situation?.color }}>{record.situation?.description}</span>;
+      }
+    }
+  ];
+
   return <>
-    <Spin tip={loading.descritivo} spinning={loading.visivel}>
-      <ModalManutencao />
-      <ModalDeConfirmacaoDeExclusao />
-      <ToastContainer />
-      <Row justify='end'>
-        <FrameBotoesPrincipais />
-      </Row>
-      <Row>
-        <Col span={4} hidden={hideSidebar}>
-          <SidebarSelecao />
+    {/*<Spin tip={loading.descritivo} spinning={loading.visivel}>*/}
+    <ModalManutencao />
+    <ModalDeConfirmacaoDeExclusao />
+    <ToastContainer />
+    <Row justify='end'>
+      <FrameCadButtons
+        inEdition={false}
+        callbackClick={(e: enBotoes) => _callbackBotoes(e)}
+        orientation={'horizontal'}
+        invisible={[enBotoes.eAlterar, enBotoes.eExcluir, enBotoes.eGravar, enBotoes.eCancelar]}
+        tooltipCaption={[{ button: enBotoes.eProcurar, caption: "Mudar modo de visualização" }]}
+      />
+    </Row>
+    <Row>
+      <Col span={hideSidebar ? 0 : 4}>
+        <Card
+          type='inner'
+          size='small'
+          title={`Detalhes (${agendamentosDoDia.length})`}
+          className="wrapper-sidebar-selection"
+          extra={(
+            <Tooltip placement='bottom' title={'Fechar o painel'}>
+              <CloseOutlined onClick={() => setHideSidebar(true)} />
+            </Tooltip>
+          )} >
+          {
+            _.map(agendamentosDoDia, (agendamento: any) => {
+              const _onClickItem = () => {
+                form_digitacao.setFieldsValue(agendamento);
+                setShowModal({ showModal: EnTipoModal.mManutencao })
+              }
+
+              return <>
+                <Tooltip title={`Clique para alterar - ${agendamento.situation?.description}`}>
+                  <Row onClick={_onClickItem} className="sidebar-selection-item" style={{ borderLeft: `4px solid ${agendamento.situation?.color}` }}>
+                    {DateUtils.dateFormatHHmm(agendamento.date) + ' ' + agendamento.person?.name}
+                  </Row>
+                </Tooltip>
+              </>
+            })
+          }
+        </Card>
+      </Col>
+      <Col span={hideSidebar ? 24 : 20}>
+        <Col span={layoutPadrao ? 0 : 24}>
+          <Table
+            pagination={false}
+            columns={colunasModeloTabela}
+            style={{ padding: '10px' }}
+            dataSource={dtSourceModeloTabela()}
+            onRow={(data: TipoDado) => {
+              return {
+                onClick: event => {
+                  console.log({ ClickTableManutencao: data })
+                  //form_digitacao.setFieldsValue(data);
+                  //setShowModal({ showModal: EnTipoModal.mManutencao })
+                }
+              }
+            }}
+          />
         </Col>
-        <Col span={hideSidebar ? 24 : 20}>
-          <MontagemDoConteudo />
+        <Col span={!layoutPadrao ? 0 : 24}>
+          <Calendar locale={locale} dateCellRender={dateCellRender} onPanelChange={panelChange} style={{ padding: '10px' }} />
         </Col>
-      </Row>
-    </Spin>
+      </Col>
+    </Row>
+    {/*</Spin> */}
   </>
 };
 
